7a2da5092db2f75b3680802930d5442bb2e6c14d
Chizoba Emeghiebo <chizobaemeh09@gmail.com>
2025-02-13 16:09:25 +0100

    basic design completed - now for responsiveness

--- src/Pages.jsx/TicketPage.jsx
+++ src/Pages.jsx/TicketPage.jsx
@@ -3,13 +3,15 @@ import Layout from "../components/Layout";
 import PageTitle from "../components/PageTitle";
 import ProgressBar from "../components/ProgressBar";
 import Barcode from "../assets/images/barcode1.png";
-import Ticket1 from "../assets/images/ticket1.png";
+import Ticket1 from "../assets/images/ticket.png";
 import Ticket2 from "../assets/images/ticket2.png";
 import ButtonContainer from "../components/ButtonContainer";
 const TicketPage = () => {
   return (
-    <div className="min-h-screen bg-vdark">
-      <Nav />
+    <div className="min-h-screen bg-vdark relative">
+      <div className="fixed top-3 w-full">
+        <Nav />
+      </div>
 
       <Layout>
         <PageTitle title="Ready" step="Step 3/3" />
@@ -23,20 +25,18 @@ const TicketPage = () => {
         </div>
 
         <div className="relative">
-          <div className="relative z-40 flex flex-col gap-6 border-2 border-border4 rounded-2xl w-[90%] mx-auto py-4">
+          <div className="relative z-40 flex flex-col gap-4 border-2 border-border4 rounded-2xl mt-12 w-[83%] md:w-[40%] lg:w-[43%] mx-auto py-2">
             <div className="relative z-40 flex flex-col items-center text-lighter gap-1">
-              <h1 className="font-heading text-4xl lg:text-6xl">
-                Techember Fest ”25
-              </h1>
-              <p className="font-display text-xs lg:text-base lg:w-[70%] lg:mx-auto">
+              <h1 className="font-heading text-4xl ">Techember Fest ”25</h1>
+              <p className="font-display text-xs lg:text-center lg:w-[70%] lg:mx-auto">
                 📍 04 Rumens road, Ikoyi, Lagos
               </p>
-              <p className="font-display text-xs lg:text-base">
+              <p className="font-display text-xs ">
                 📅 March 15, 2025 | 7:00 PM
               </p>
             </div>
             {/* image holder */}
-            <div className="relative z-40 border-2 border-border3 rounded-xl w-1/2 mx-auto p-20 bg-amber-200"></div>
+            <div className="relative z-40 border-2 border-border3 rounded-xl w-1/2 mx-auto p-16 bg-amber-200"></div>
             {/* information */}
             <div className="relative z-40 w-[90%] mx-auto grid grid-cols-2 justify-between items-stretch px-2 border-2 border-border3 rounded-lg text-white">
               <div className="p-2 border-b-2 border-r-2 border-border3 ">
@@ -61,15 +61,16 @@ const TicketPage = () => {
               </div>
             </div>
           </div>
+
           <img
-            src={Ticket2}
+            src={Ticket1}
             alt="ticket background"
-            className="absolute top-[63%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
+            className="flex absolute -top-4 md:top-[62%] lg:top-[63%] md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 z-10"
           />
           <img
             src={Barcode}
             alt="barcode"
-            className="absolute -bottom-[35%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
+            className="absolute -bottom-[30%] md:-bottom-[33%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
           />
         </div>
 
